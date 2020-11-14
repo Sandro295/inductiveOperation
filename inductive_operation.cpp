5e45386e89de15d2a028ad0076bd0925ac4c63a8
Sandro <danil.efimov.295@mail.ru>
2020-11-14 15:16:52 +0300

    deleted debugging code

--- inductive_operation.cpp
+++ inductive_operation.cpp
@@ -8,29 +8,6 @@ int main() {
 	cin >> params.upperBound;
 	MaxFinder operation;
 	maxFinderInit (operation, params);
-	
-
-	MaxFinderInputItem input;
-	cin >> input.x;
-	MaxFinderOutput output;
-	MaxFinderOutput output1;
-	maxFinderProcess(operation, input, output);
-
-	
-		for (unsigned i = 0; i < output.count; ++i) {
-			cout << output.sequence[i].currentMax << " - " << output.sequence[i].amountMax << endl;
-		}
-	
-
-	cin >> input.x;
-	maxFinderProcess(operation, input, output1);
-	
-		for (unsigned i = 0; i < output1.count; ++i) {
-			cout << output1.sequence[i].currentMax << " - " << output1.sequence[i].amountMax << endl;
-		}
-	
-
-
 
 	while (true) {
 		MaxFinderInputItem input;
